1d23798b349778a1cee7177c269c41225a3057de
Aiden Mori-Lair <167143359+akira23456@users.noreply.github.com>
2026-09-15 12:21:43 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] == target:
+                return left
+            elif nums[right] == target:
+                return right
+            else:
+                left += 1
+                right -= 1
+        
+        return -1
+
+        
+
+            
